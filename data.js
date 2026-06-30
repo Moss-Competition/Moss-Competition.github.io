f6daf40dced14ac3f30664347ae657482fb0300e
Moss-Competition <moss.competition@proton.me>
2026-07-01 00:22:01 +0300

    First Commit

---
+++ data.js
@@ -0,0 +1,202 @@
+const data = `
+{
+  "2026" : {
+    "tracks": [
+      {
+        "track_name": "Hitomidori",
+        "author": "ki-zu",
+        "track_cover_dir": "Media/Covers/2026/Hitomidori.jpg",
+        "track_dir": "Media/Tracks/2026/ki-zu_-_Hitomidori.mp3",
+        "person" : "vdskkk",
+        "country" : "Япония",
+        "flag_dir" : "Media/Flags/japan.png",
+        "points" : 0
+      },
+        {
+        "track_name": "Tir na nog",
+        "author": "Celtic Woman, Oonagh",
+        "track_cover_dir": "Media/Covers/2026/tir_na_nog.jpg",
+        "track_dir": "Media/Tracks/2026/Celtic_Woman_-_Tir_na_nog.mp3",
+        "person" : "Eike",
+        "country" : "Ирландия",
+        "flag_dir" : "Media/Flags/Ireland.png",
+        "points" : 0
+      },
+      {
+        "track_name": "Miss You",
+        "author": "Oliver Tree, Robin Schulz",
+        "track_cover_dir": "Media/Covers/2026/Miss_You.jpg",
+        "track_dir": "Media/Tracks/2026/Oliver_Tree_&_Robin_Schulz_-_Miss_You.mp3",
+        "person" : "MaxxTheLightning",
+        "country" : "США",
+        "flag_dir" : "Media/Flags/USA.png",
+        "points" : 0
+      },
+      {
+        "track_name": "West end kids",
+        "author": "New Politics",
+        "track_cover_dir": "Media/Covers/2026/west_end_kids.jpg",
+        "track_dir": "Media/Tracks/2026/New_Politics_-_West_End_Kids.mp3",
+        "person" : "Aamosade",
+        "country" : "Дания",
+        "flag_dir" : "Media/Flags/denmark.png",
+        "points" : 0
+      },
+      {
+        "track_name": "Don't Stop Me Now",
+        "author": "Queen",
+        "track_cover_dir": "Media/Covers/2026/Dont_stop_me_now.jpg",
+        "track_dir": "Media/Tracks/2026/Queen_-_Dont_Stop_Me_Now.mp3",
+        "person" : "Mutt",
+        "country" : "Великобритания",
+        "flag_dir" : "Media/Flags/UK.png",
+        "points" : 0
+      },
+      {
+        "track_name": "NO BATIDAO",
+        "author": "ZXKAI",
+        "track_cover_dir": "Media/Covers/2026/no_batidao.jpg",
+        "track_dir": "Media/Tracks/2026/ZXKAI_-_NO_BATIDAO.mp3",
+        "person" : "enya",
+        "country" : "Бразилия",
+        "flag_dir" : "Media/Flags/Brazil.png",
+        "points" : 0
+      },
+      {
+        "track_name": "Self Aware",
+        "author": "Temper City",
+        "track_cover_dir": "Media/Covers/2026/Self_Aware.jpg",
+        "track_dir": "Media/Tracks/2026/Temper_City_-_Self_Aware.mp3",
+        "person" : "Takos",
+        "country" : "Израиль",
+        "flag_dir" : "Media/Flags/Israel.png",
+        "points" : 0
+      },
+      {
+        "track_name": "You are the only one",
+        "author": "Сергей Лазарев",
+        "track_cover_dir": "Media/Covers/2026/you_are_the_only_one.jpg",
+        "track_dir": "Media/Tracks/2026/SERGEJ_LAZAREV_-_You_are_the_only_one.mp3",
+        "person" : "vatrushka_tyt",
+        "country" : "Россия",
+        "flag_dir" : "Media/Flags/Russia.png",
+        "points" : 0
+      },
+      {
+        "track_name": "GIRL HELL 1999",
+        "author": "femtanyl",
+        "track_cover_dir": "Media/Covers/2026/girl_hell_1999.jpg",
+        "track_dir": "Media/Tracks/2026/femtanyl_-_GIRL_HELL_1999.mp3",
+        "person" : "Myatka",
+        "country" : "Канада",
+        "flag_dir" : "Media/Flags/Canada.png",
+        "points" : 0
+      },
+      {
+        "track_name": "Torna a casa",
+        "author": "Maneskin",
+        "track_cover_dir": "Media/Covers/2026/torna_a_casa.jpg",
+        "track_dir": "Media/Tracks/2026/Maneskin_-_Torna_a_casa.mp3",
+        "person" : "Reinsell",
+        "country" : "Италия",
+        "flag_dir" : "Media/Flags/Italy.png",
+        "points" : 0
+      },
+      {
+        "track_name": "We are the people",
+        "author": "Empire of the Sun",
+        "track_cover_dir": "Media/Covers/2026/empire_of_the_sun.jpg",
+        "track_dir": "Media/Tracks/2026/Empire_Of_The_Sun_-_We_Are_The_People.mp3",
+        "person" : "Rodion",
+        "country" : "Австралия",
+        "flag_dir" : "Media/Flags/Australia.png",
+        "points" : 0
+      }
+    ],
+    "clips": []
+  },
+  "2025" : {
+    "tracks": [
+      {
+        "track_name": "Gossip",
+        "author": "Måneskin",
+        "track_cover_dir": "Media/Covers/2025/Gossip.jpg",
+        "track_dir": "Media/Tracks/2025/Maneskin-Gossip.mp3",
+        "person" : "Schrödinger's Rodion",
+        "country" : "Италия",
+        "flag_dir" : "Media/Flags/Italy.png",
+        "points" : 63
+      },
+      {
+        "track_name": "Under Your Spell",
+        "author": "Snow Strippers",
+        "track_cover_dir": "Media/Covers/2025/Under_Your_Spell.jpg",
+        "track_dir": "Media/Tracks/2025/Snow_Strippers-Under_Your_Spell.mp3",
+        "person" : "Takos",
+        "country" : "США",
+        "flag_dir" : "Media/Flags/USA.png",
+        "points" : 61
+      }
+    ],
+    "clips": [
+      {
+        "clip_name": "Gossip",
+        "author": "Måneskin",
+        "clip_dir": "Media/Clips/2025/Gossip.mp4",
+        "preview_dir": "Media/Previews/2025/Gossip.png"
+      },
+      {
+        "clip_name": "Under Your Spell",
+        "author": "Snow Strippers",
+        "clip_dir": "Media/Clips/2025/Under_Your_Spell.mp4",
+        "preview_dir": "Media/Previews/2025/Under_Your_Spell.png"
+      }
+    ]
+  },
+  "accounts": [
+    {
+      "username": "vdskkk",
+      "password": "123456"
+    },
+    {
+      "username": "MaxxTheLightning",
+      "password": "123456"
+    },
+    {
+      "username": "Aamosade",
+      "password": "123456"
+    },
+    {
+      "username": "Mutt",
+      "password": "123456"
+    },
+    {
+      "username": "enya",
+      "password": "123456"
+    },
+    {
+      "username": "Takos",
+      "password": "123456"
+    },
+    {
+      "username": "vatryshka_tyt",
+      "password": "123456"
+    },
+    {
+      "username": "Reinsell",
+      "password": "123456"
+    },
+    {
+      "username": "Rodion",
+      "password": "123456"
+    },
+    {
+      "username": "Eike",
+      "password": "123456"
+    },
+    {
+      "username": "Myatka",
+      "password": "123456"
+    }
+  ]
+}`
